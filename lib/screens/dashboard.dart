cbbca13f85d085b21e640be4f4d4da915a61be70
Muhammad Atif Pervaiz <matifpervaizofficial@gmail.com>
2023-07-27 17:47:38 +0500

    privacy-policy

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:motplay/screens/configration.dart';
 import 'package:motplay/screens/dmca.dart';
+import 'package:motplay/screens/privacy-policy.dart';
 import 'package:xml/xml.dart';
 import '../test/html_view.dart';
 import '../utils/constanst.dart';
@@ -220,6 +221,10 @@ class _DashboardState extends State<Dashboard> {
               thickness: 0.5,
             ),
             ListTile(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (ctx) => PrivacyPolicyScreen()));
+              },
               leading: ImageIcon(AssetImage("assets/images/icon2.png")),
               title: Text(
                 "Politicas de Privacidad",
---
+++ lib/screens/privacy-policy.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:motplay/utils/mycolors.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PrivacyPolicyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        backgroundColor: blue,
+        title: Text(
+          'Política de Privacidad',
+          style: TextStyle(fontSize: 32),
+        ),
+        iconTheme: IconThemeData(color: Colors.grey.shade200),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: [
+          Text(
+            "Política de Privacidad y Contenido",
+            style: TextStyle(fontSize: 24, color: blue),
+          ),
+          ListTile(
+            title: Text(
+                'La app no recoge ningún dato de carácter personal del usuario como, por ejemplo, nombre, fotografías o localización.'),
+          ),
+          ListTile(
+            title: Text(
+                'En consecuencia, la app no comparte ningún dato personal con ninguna otra entidad o terceras personas.'),
+          ),
+          ListTile(
+            title: Text(
+                'Las imágenes y los videos introducidos por el usuario son enviados al servidor imgs1.e-droid.net con el fin de poder ser recuperados posteriormente por el propio usuario, y con el fin de que la app pueda ofrecer las funcionalidades según su descripción.'),
+          ),
+          ListTile(
+            title: Text(
+                'Permitimos que terceras compañías publiquen anuncios y recopilen cierta información anónima cuando visite nuestra aplicación. Estas empresas pueden utilizar información anónima, como su ID de publicidad de Google, el tipo y la versión de su dispositivo, la actividad de navegación, la ubicación y otros datos técnicos relacionados con su dispositivo, a fin de proporcionar anuncios.'),
+          ),
+          ListTile(
+            title: Text(
+                'Si desea obtener más información sobre cómo estas empresas utilizan su información y cómo darse de baja de ciertas funciones publicitarias, puede visitar los siguientes enlaces:'),
+          ),
+        ],
+      ),
+    );
+  }
+}
