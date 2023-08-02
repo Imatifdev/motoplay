fcf42a27b0dd75189686efd94241fd0127459be4
Abdullah Ayaz <106861398+abd529@users.noreply.github.com>
2023-08-02 16:26:03 +0500

    updated lists

--- lib/utils/mycolors.dart
+++ lib/utils/mycolors.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const Color blue = Color(0xff36C1F1);
 const Color seaGreen = Color.fromRGBO(48,178,194,1);
+const Color gradBlue = Color.fromRGBO(0, 87, 150, 1);
+const Color gradOrange =  Color.fromRGBO(255, 107, 0, 1);
