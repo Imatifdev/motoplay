d87ffbc47c0718b822546a73ed3886f4c7637641
Muhammad Atif Pervaiz <matifpervaizofficial@gmail.com>
2023-08-02 16:58:52 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -20,11 +20,10 @@ class MyApp extends StatelessWidget {
       title: 'Moto Play',
       theme: ThemeData(
         fontFamily: GoogleFonts.teko().fontFamily,
-        //pri
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: NewHome(),
+      home: Dashboard(),
     );
   }
 }
